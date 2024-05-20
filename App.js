c6402eae97b2f227785b430661cf4dd9e0733414
Robotwars1 <magnus.carlsson4@gmail.com>
2024-05-20 18:49:11 +0200

    Feat. Images for if wr tie

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const SpMapNames = ["Portal Gun", "Smooth Jazz", "Cube Momentum", "Future Starte
 const CoopMapNames = ["Doors", "Buttons", "Lasers", "Rat Maze", "Laser Crusher", "Behind The Scenes", "Flings", "Infinifling", "Team Retrieval", "Vertical Flings", "Catapults", "Multifling", "Fling Crushers", "Industrial Fan", "Cooperative Bridges", "Bridge Swap", "Fling Block", "Catapult Block", "Bridge Fling", "Turret Walls", "Turret Assassin", "Bridge Testing", "Cooperative Funnels", "Funnel Drill", "Funnel Catch", "Funnel Laser", "Cooperative Polarity", "Funnel Hop", "Advanced Polarity", "Funnel Maze", "Turret Warehouse", "Repulsion Jumps", "Double Bounce", "Bridge Repulsion", "Wall Repulsion", "Propulsion Crushers", "Turret Ninja", "Propulsion Retrieval", "Vault Entrance", "Separation", "Triple Axis", "Catapult Catch", "Bridge Gels", "Maintenance", "Bridge Catch", "Double Lift", "Gel Maze", "Crazier Box"];
 
 const MapNameLabels = document.getElementsByClassName("MapLabel");
-const RecordLabels = document.getElementsByClassName("WrLabel");
+const RecordTieMarkers = document.getElementsByClassName("WrTie");
 const Top5000Labels = document.getElementsByClassName("Top5000Label");
 
 window.onload = async function() {
@@ -30,10 +30,10 @@ async function UpdateLabels() {
     // Top 5000
     for (let i = 0; i < Top5000Labels.length; i++) {
         if (i < Sp5000.length) {
-            Top5000Labels[i].textContent = `Top 5000: ${Sp5000[i]}`;
+            Top5000Labels[i].textContent = Sp5000[i];
         }
         else {
-            Top5000Labels[i].textContent = `Top 5000: ${Coop5000[i - Sp5000.length]}`;
+            Top5000Labels[i].textContent = Coop5000[i - Sp5000.length];
         }
     }
 
@@ -47,14 +47,23 @@ async function UpdateLabels() {
     let SPWr = [];
     let CoopWr = [];
 
-    for (let i = 0; i < RecordLabels.length; i++) {
+    for (let i = 0; i < RecordTieMarkers.length; i++) {
         if (i < SpData.length) {
-            RecordLabels[i].textContent = `WR: ${SpData[i]}`;
+            RecordTieMarkers[i].src = `${CheckRecordTie(SpData[i]), Sp5000[i]}.png`;
             SPWr.push(SpData[i]);
         }
         else {
-            RecordLabels[i].textContent = `WR: ${CoopData[i - SpData.length]}`;
+            RecordTieMarkers[i].textContent = `${CheckRecordTie(CoopData[i - SpData.length], Coop5000[i - SpData.length])}.png`;
             CoopWr.push(CoopData[i]);
         }
+    }
+}
+
+function CheckRecordTie(WR, Top5000) {
+    if (WR == Top5000) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
