c00df86c79c1556779d9779bc6c60400b6ef8ffd
Robotwars1 <magnus.carlsson4@gmail.com>
2024-09-16 20:25:44 +0200

    Feat. Better accuracy with count shown

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ const Coop5000 = [2, 7, 8, 4, 2, 0, 9, 6, 5, 4, 15, 11, 8, 12, 11, 8, 5, 12, 18,
 const SpMapNames = ["Portal Gun", "Smooth Jazz", "Cube Momentum", "Future Starter", "Incinerator", "Laser Stairs", "Dual Lasers", "Laser Over Goo", "Trust Fling", "Pit Flings", "Fizzler Intro", "Ceiling Catapult", "Ricochet", "Bridge Intro", "Bridge the Gap", "Turret Intro", "Laser Relays", "Turret Blocker", "Laser vs Turret", "Pull the Rug", "Column Blocker", "Laser Chaining", "Triple Laser", "Jail Break", "Escape", "Turret Factory", "Turret Sabotage", "Neurotoxin Sabotage", "Underground", "Cave Johnson", "Repulsion Intro", "Bomb Flings", "Crazy Box", "PotatOS", "Propulsion Intro", "Propulsion Flings", "Conversion Intro", "Three Gels", "Funnel Intro", "Ceiling Button", "Wall Button", "Polarity", "Funnel Catch", "Stop the Box", "Laser Catapult", "Laser Platform", "Propulsion Catch", "Repulsion Polarity", "Finale 2", "Finale 3", "Finale 4"];
 const CoopMapNames = ["Doors", "Buttons", "Lasers", "Rat Maze", "Laser Crusher", "Behind The Scenes", "Flings", "Infinifling", "Team Retrieval", "Vertical Flings", "Catapults", "Multifling", "Fling Crushers", "Industrial Fan", "Cooperative Bridges", "Bridge Swap", "Fling Block", "Catapult Block", "Bridge Fling", "Turret Walls", "Turret Assassin", "Bridge Testing", "Cooperative Funnels", "Funnel Drill", "Funnel Catch", "Funnel Laser", "Cooperative Polarity", "Funnel Hop", "Advanced Polarity", "Funnel Maze", "Turret Warehouse", "Repulsion Jumps", "Double Bounce", "Bridge Repulsion", "Wall Repulsion", "Propulsion Crushers", "Turret Ninja", "Propulsion Retrieval", "Vault Entrance", "Separation", "Triple Axis", "Catapult Catch", "Bridge Gels", "Maintenance", "Bridge Catch", "Double Lift", "Gel Maze", "Crazier Box"];
 
+// 3 Arrays of elements of equal length (Equal length part being what big for loop under is based on)
 const MapNameLabels = document.getElementsByClassName("MapLabel");
 const RecordTieMarkers = document.getElementsByClassName("WrTie");
 const Top5000Labels = document.getElementsByClassName("Top5000Label");
@@ -17,27 +18,6 @@ window.onload = async function() {
 }
 
 async function UpdateLabels() {
-    // Map Name
-    for (let i = 0; i < MapNameLabels.length; i++) {
-        if (i < SpMapNames.length) {
-            MapNameLabels[i].textContent = SpMapNames[i];
-        }
-        else {
-            MapNameLabels[i].textContent = CoopMapNames[i - SpMapNames.length];
-        }
-    }
-
-    // Top 5000
-    for (let i = 0; i < Top5000Labels.length; i++) {
-        if (i < Sp5000.length) {
-            Top5000Labels[i].textContent = Sp5000[i];
-        }
-        else {
-            Top5000Labels[i].textContent = Coop5000[i - Sp5000.length];
-        }
-    }
-
-    // WR
     // Parse Jsons
     let SpJson = await fetch(SpRecords);
     let SpData = await SpJson.json();
@@ -47,14 +27,47 @@ async function UpdateLabels() {
     let SPWr = [];
     let CoopWr = [];
 
-    for (let i = 0; i < RecordTieMarkers.length; i++) {
+    // Set all elements
+    for (let i = 0; i < MapNameLabels.length; i++) {
+        // WR Indicator
+        let RecordTie = false;
+
         if (i < SpData.length) {
-            RecordTieMarkers[i].src = `${CheckRecordTie(SpData[i], Sp5000[i])}.png`;
+            RecordTie = CheckRecordTie(SpData[i], Sp5000[i]);
+            RecordTieMarkers[i].src = `${RecordTie}.png`;
             SPWr.push(SpData[i]);
         }
         else {
-            RecordTieMarkers[i].src = `${CheckRecordTie(CoopData[i - SpData.length], Coop5000[i - SpData.length])}.png`;
+            RecordTie = CheckRecordTie(CoopData[i - SpData.length], Coop5000[i - SpData.length]);
+            RecordTieMarkers[i].src = `${RecordTie}.png`;
             CoopWr.push(CoopData[i]);
+        }
+
+        // Map Name
+        if (i < SpMapNames.length) {
+            MapNameLabels[i].textContent = SpMapNames[i];
+        }
+        else {
+            MapNameLabels[i].textContent = CoopMapNames[i - SpMapNames.length];
+        }
+
+        // Top 5000
+        // If not RecordTie lower count by 1 to make it more likely that the shown number is what is actually required to get onto lp boards
+        if (i < Sp5000.length) {
+            if (RecordTie) {
+                Top5000Labels[i].textContent = Sp5000[i];
+            }
+            else {
+                Top5000Labels[i].textContent = Sp5000[i] - 1;
+            }
+        }
+        else {
+            if (RecordTie) {
+                Top5000Labels[i].textContent = Coop5000[i - Sp5000.length];
+            }
+            else {
+                Top5000Labels[i].textContent = Coop5000[i - Sp5000.length] - 1;
+            }
         }
     }
 }
